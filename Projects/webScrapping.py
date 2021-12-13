f9815e7d482c41bab68dcc590f493b05c35c68c6
Anastasiia-web <anastasiyamarket@gmail.com>
2021-12-14 00:31:50 +0200

    updates

--- Projects/webScrapping.py
+++ Projects/webScrapping.py
@@ -20,6 +20,7 @@ link = link_to_certification.get('href')
 article = soup.find_all("p")
 
 exception_1 = "Create a class named MyClass:"
+
 exception_2 = "Start the Exercise"
 exception_3 = "We just launchedW3Schools videos"
 exception_4 = "If you want to report an error, or if you want to make a suggestion, do not hesitate to send us an e-mail:"
@@ -31,8 +32,8 @@ list_exceptions = [exception_1, exception_2, exception_3, exception_4, exception
 print(notes_title)
 
 for sentence in article:
-    almost_ready_notes = sentence.get_text()
-    if almost_ready_notes not in list_exceptions:
-        print(almost_ready_notes)
+    ready_notes = sentence.get_text()
+    if ready_notes not in list_exceptions:
+        print(ready_notes)
 
 print(link)
