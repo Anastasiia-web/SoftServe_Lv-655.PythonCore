00d9b356aa4acff36a4258705425351aff0adb59
Anastasiia-web <anastasiyamarket@gmail.com>
2021-12-16 16:26:47 +0200

    Notes are added

---
+++ Course_notes/classes.py
@@ -0,0 +1,15 @@
+#My drafts
+
+# def list_animals(animals):   # SUBMITED))) but not adopted for new animals in the list !!!
+#     list = []
+#     for i in animals:
+#         list = str("1. " + i + "\n2. " + animals[1] + "\n3. " + animals[2] + "\n")
+#         return list
+
+# print(list_animals([ 'dog', 'cat', 'elephant' ]))
+
+# def list_animals(animals):                   
+#     list = ''
+#     for i in range(len(animals)):                     # SOLUTION add "len"
+#         list += str(i + 1) + '. ' + animals[i] + '\n'
+#     return list
