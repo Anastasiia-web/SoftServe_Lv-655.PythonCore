cd85244eec84391eec888bb30df10f81d697cbb2
Anastasiia-web <anastasiyamarket@gmail.com>
2021-12-25 19:49:03 +0200

    updated

---
+++ Course_notes/practice_4.notes.py
@@ -0,0 +1,44 @@
+t = 'sdf'
+t.strip()    # не присваиваем => будет такое же : answer : sdf
+print(t)
+
+t = 'sdf'
+s = t.strip("f")    # !!! присваиваем => будет обрезано : answer : sd
+print(s)
+
+import math
+c = math.floor(5.5)   # answer : 5 в сторону меньшего!!!
+print(c)
+
+
+
+
+
+
+
+
+
+##############
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Course_notes/t_2.py
@@ -0,0 +1,22 @@
+### линк на задачи по sql Salesforce
+
+### https://proghub.ru/q/38d5e3
+
+## Inner Join возвращает только значения, если совпадение есть во всех четырех таблицах !!!
+
+## https://overcoder.net/q/3118973/inner-join-%D0%B2%D0%BE%D0%B7%D0%B2%D1%80%D0%B0%D1%89%D0%B0%D0%B5%D1%82-%D1%82%D0%BE%D0%BB%D1%8C%D0%BA%D0%BE-%D0%B7%D0%BD%D0%B0%D1%87%D0%B5%D0%BD%D0%B8%D1%8F-%D0%B5%D1%81%D0%BB%D0%B8-%D1%81%D0%BE%D0%B2%D0%BF%D0%B0%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5-%D0%B5%D1%81%D1%82%D1%8C-%D0%B2%D0%BE-%D0%B2%D1%81%D0%B5%D1%85-%D1%87%D0%B5%D1%82%D1%8B%D1%80%D0%B5%D1%85
+
+## УЧИТЬ ПРО SQL !!!
+### https://coderoad.ru/38549/%D0%92-%D1%87%D0%B5%D0%BC-%D1%80%D0%B0%D0%B7%D0%BD%D0%B8%D1%86%D0%B0-%D0%BC%D0%B5%D0%B6%D0%B4%D1%83-INNER-JOIN-%D0%B8-%D0%B2%D0%BD%D0%B5%D1%88%D0%BD%D0%B8%D0%BC-JOIN
+
+### join ОТЛИЧИЯ С КАРТИНКАМИ)
+### http://www.skillz.ru/dev/php/article-Obyasnenie_SQL_obedinenii_JOIN_INNER_OUTER.html
+
+# result = (80 - 12*5) / 4
+# print(result)
+# print(r"\nwoow")   5.0
+# \nwoow
+
+f = {1:'2'}
+f = {}
+print(len(f))
